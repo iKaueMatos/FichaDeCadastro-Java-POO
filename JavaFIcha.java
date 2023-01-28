702c215bddab55ee02d5934e8a318456d41cad81
ikauematos <kaue178hotimail@gmail.com>
2023-01-28 07:09:44 -0300

    Projeto finalizado

--- JavaFIcha.java
+++ JavaFIcha.java
@@ -7,43 +7,48 @@ import java.util.logging.Logger;
 
 public class JavaFIcha<leitura> {
     public static void main(String[] args) {
-    String nome;
-    int idade;
+    String nome = null;
+    int idade = 0;
 
-    char sexo;
+    char sexo = 0;
 
-    float altura;
+    float altura = 0;
 
-    float peso;
+    float peso = 0;
 
 
-
-//Objetos
-        Scanner leitura = new Scanner(System.in);
-
-        System.out.println("Digite seu nome: ");
-        nome = leitura.nextLine();
-
-        System.out.println("Digite sua idade: ");
-        idade = leitura.nextInt();
-
-        System.out.println("Digite sua altura: ");
-        altura = leitura.nextFloat();
-
-        System.out.println("Digite seu peso: ");
-        peso = leitura.nextFloat();
-
-        System.out.println("Digite seu sexo: ");
-
-        // try 1 tratamento de erros sexo;
-        //2 try tratamento de erros
       try {
-          sexo = (char) System.in.read();
           Pessoa pessoaDadosFicha = new Pessoa(nome,peso,sexo,altura,idade);
+
+          Scanner leitura = new Scanner(System.in);
+
+          System.out.println("Digite seu nome: ");
+          nome = leitura.nextLine();
+          pessoaDadosFicha.setNome(nome);
+
+          System.out.println("Digite sua idade: ");
+          idade = leitura.nextInt();
+          pessoaDadosFicha.setIdade(idade);
+          System.out.println("Digite sua altura: ");
+          altura = leitura.nextFloat();
+          pessoaDadosFicha.setAltura(altura);;
+
+          System.out.println("Digite seu peso: ");
+          peso = leitura.nextFloat();
+          pessoaDadosFicha.setPeso(peso);
+
+          System.out.println("Digite seu sexo: ");
+          pessoaDadosFicha.setSexo(sexo);
+          pessoaDadosFicha.imprimirDados();
+
+          sexo = (char) System.in.read();
+
           pessoaDadosFicha.imprimirDados();
 
       } catch(IOException ex) {
           Logger.getLogger(JavaFIcha.class.getName()).log(Level.SEVERE, null, ex);
+      }catch (IllegalArgumentException ex){
+          System.out.println(ex.getMessage());
       }
     }
 }
